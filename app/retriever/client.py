7d6b041be6974d66023fcc047b78a214b7ca82ff
sleeplessuser <52459470+sleeplessuser@users.noreply.github.com>
2024-11-09 21:26:04 +0300

    feat: heatmap calculation with retriever api

--- app/retriever/client.py
+++ app/retriever/client.py
@@ -1,10 +1,12 @@
 import numpy as np
 import requests
+from PIL import Image
 
 
 class ColpaliClient:
     IMAGE_ROUTE = "embed_images"
     TEXT_ROUTE = "embed_texts"
+    HEATMAPS_ROUTE = "heatmaps"
     EMBEDDING_DIM = 128
 
     def __init__(self, host: str, port: int):
@@ -38,3 +40,16 @@ class ColpaliClient:
         except Exception:
             embeds = None
         return embeds
+
+    def get_heatmap(self, query: str, image: str) -> np.ndarray | None:
+        url = f"{self.url}/{self.HEATMAPS_ROUTE}/"
+        response = requests.post(
+            url, params={"query": query}, files={"image": open(image, "rb")}
+        )
+        try:
+            image_size = Image.open(image).size
+            data = np.frombuffer(response.content, dtype=np.float64)
+            heatmap = data.reshape(*image_size[::-1], 1)
+        except Exception:
+            heatmap = None
+        return heatmap
